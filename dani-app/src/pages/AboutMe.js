77ec996037a3f569ac4e235d03e2ad12272a4bee
agusnarvaez <agus.narvaez@outlook.com>
2022-04-30 05:36:44 -0300

    Estilo about para notebook y tablet

--- dani-app/src/pages/AboutMe.js
+++ dani-app/src/pages/AboutMe.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import "../assets/styles/about.css"
 function AboutMe() {
     return (
-        <div>
-            <div>
-            <h2>Sobre Mi</h2>
-            <p>Persona Íntegra y con habilidades para las relaciones interpersonales. Me gustaría afrontar un trabajo que permita desarrollarme y crecer profesionalmente con el objetivo de profundizar mis conocimientos. Me consideron una persona responsable, con actitud positiva y buena capacidad de aprendizaje y trabajo en equipo.
-            </p>
+        <div className="aboutPage">
+            <div className="about">
+                <h2 className="tituloAbout">Sobre Mi</h2>
+                <p>Persona Íntegra y con habilidades para las relaciones interpersonales. Me gustaría afrontar un trabajo que permita desarrollarme y crecer profesionalmente con el objetivo de profundizar mis conocimientos. Me consideron una persona responsable, con actitud positiva y buena capacidad de aprendizaje y trabajo en equipo.
+                </p>
             </div>
-            <div><img className="coverImg"
-                src="../../public/assets/images/about.png"
-                alt="cover"
-            /></div>
-            <p>hola hola</p>
-        </div>
+            <div className="aboutCover">
+                <img
+                    src="../assets/images/background/aboutCover.png"
+                    alt="aboutCover"
+                />
+            </div>
+        </div >
     )
 }
 
--- dani-app/src/pages/Contact.js
+++ dani-app/src/pages/Contact.js
@@ -1,9 +1,10 @@
 
 import React, { useEffect, useState } from "react";
 import emailjs from 'emailjs-com'
+import "../assets/styles/contact.css"
 
 function Contact() {
-    const frmContact = { from_name: '', from_email: '', company: '', reason: '', message: '' };
+    const frmContact = { from_name: '', from_email: '', phone: '', reason: '', message: '', attachment: '' };
     const [contact, setContact] = useState(frmContact);
     const [showMessage, setShowMessage] = useState(false);
     const handleChange = e => {
@@ -23,49 +24,66 @@ function Contact() {
             });
     }
     return (
-        <div className="container pt-2 text-center">
+        <main>
+            <div className="formContainer">
 
-            {showMessage ? <div className="alert alert-success col-md-5 mx-auto" role="alert">Email Send Success!!</div> : ``}
+                <form onSubmit={handleSubmit}>
+                    <h1> Contacto</h1>
+                    <input
+                        required
+                        type="text"
+                        value={contact.from_name} name="from_name"
+                        onChange={handleChange} className="smallInput"
+                        placeholder="Nombre"
+                    />
 
-            <form onSubmit={handleSubmit}>
-                <div className="pt-3"><h3 className="font-weight-bold"> Contactate conmigo!! </h3></div>
-                <div className="pt-3 col-md-5 mx-auto">
-                    <div className="form-group text-left"> <b>Nombre</b> <br />
-                        <input required type="text" value={contact.from_name} name="from_name" onChange={handleChange} className="form-control" placeholder="Your email" />
-                    </div>
-                </div>
-                <div className="pt-3 col-md-5 mx-auto">
-                    <div className="form-group text-left"> <b>Razón</b> <br />
-                        <select required className="form-control" value={contact.reason} onChange={handleChange} name="reason">
-                            <option value='' >Select</option>
-                            <option value="info" >Info</option>
-                            <option value="buy">Buy</option>
-                            <option value="play tennis">Play tennis</option>
-                            <option value="other">Other</option>
-                        </select>
-                    </div>
-                </div>
-                <div className="pt-3 col-md-5 mx-auto">
-                    <div className="form-group text-left"> <b>Email</b> <br />
-                        <input value={contact.from_email} required type="text" name="from_email" onChange={handleChange} className="form-control" placeholder="Your email" />
-                    </div>
-                </div>
-                <div className="pt-3 col-md-5 mx-auto">
-                    <div className="form-group text-left"> <b>Compania</b> <br />
-                        <input value={contact.company} required type="text" name="company" onChange={handleChange} className="form-control" placeholder="Your email" />
-                    </div>
-                </div>
-                <div className="pt-3 col-md-5 mx-auto">
-                    <div className="form-group text-left"> <b>Describe your concerns</b> <br />
-                        <textarea required name="message" onChange={handleChange} className="form-control" placeholder="Describe your concerns" value={contact.message}></textarea>
-                    </div>
-                </div>
-                <div className="pt-3 col-md-5 mx-auto text-left">
-                    <button className="btn btn-primary">Submit</button>
-                </div>
-            </form >
+                    <input
+                        value={contact.phone} required type="text" name="phone" onChange={handleChange} className="smallInput" placeholder="Telefono"
+                    />
 
-        </div >
+                    <input
+                        required
+                        value={contact.from_email} type="text" name="from_email" onChange={handleChange} className="largeInput"
+                        placeholder="Email"
+                    />
+
+
+
+                    <input
+                        required
+                        className="largeInput"
+                        value={contact.reason}
+                        type="text"
+                        name="reason"
+                        onChange={handleChange} placeholder="Motivo de contacto"
+                    />
+
+
+
+                    <textarea
+                        required
+                        className="largeInput" name="message"
+                        onChange={handleChange}
+                        placeholder="Mensaje"
+                        value={contact.message}
+                    />
+
+                    <input type="file"></input>
+
+                    <button className="submit">Enviar</button>
+
+
+                    {showMessage ? <div className="alertSuccess" role="alert">Enviado con éxito!</div> : ``}
+                </form >
+
+            </div >
+            <div className="contactCover">
+                <img
+                    src="../assets/images/background/contactCover.png"
+                    alt="contactCover"
+                />
+            </div>
+        </main >
     )
 }
 
