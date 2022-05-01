9ff4ec05eb5c2656c464feed240ee1f1aa0ce9df
agusnarvaez <agus.narvaez@outlook.com>
2022-05-01 11:11:09 -0300

    Estilo página Contacto, para Tablet y Desktop

--- dani-app/src/components/App.js
+++ dani-app/src/components/App.js
@@ -26,6 +26,17 @@ function App() {
         <Route path='/projects/:id' element={<Project />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='*' element={<NotFound404 />} />
+        <Route path="/linkedIn" component={() => {
+          window.location.href = "https://www.linkedin.com/in/daniela-laiolo-2a4000140/";
+          return null;
+        }} />
+        <Route
+          path="/privacy-policy"
+          component={() => {
+            window.location.replace('https://example.com/1234');
+            return null;
+          }}
+        />
       </Routes>
 
     </BrowserRouter>
--- dani-app/src/pages/Contact.js
+++ dani-app/src/pages/Contact.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import emailjs from 'emailjs-com'
 import "../assets/styles/contact.css"
-
+import { NavLink } from 'react-router-dom';
 function Contact() {
     const frmContact = { from_name: '', from_email: '', phone: '', reason: '', message: '', attachment: '' };
     const [contact, setContact] = useState(frmContact);
@@ -57,9 +57,6 @@ function Contact() {
                         name="reason"
                         onChange={handleChange} placeholder="Motivo de contacto"
                     />
-
-
-
                     <textarea
                         required
                         className="largeInput" name="message"
@@ -67,15 +64,54 @@ function Contact() {
                         placeholder="Mensaje"
                         value={contact.message}
                     />
+                    <div className="attachment">
+                        <span class='attachment'>
+                            <input type="file" name="attachment" class="" id="attachment" />
+                        </span>
+                        <label for="attachment" class='labelAttachment'>
+                            <span ><img src="../assets/images/icons/attachment.svg" /><p>Adjuntar archivo</p></span>
+                        </label>
+                    </div>
 
-                    <input type="file"></input>
 
                     <button className="submit">Enviar</button>
 
 
                     {showMessage ? <div className="alertSuccess" role="alert">Enviado con éxito!</div> : ``}
                 </form >
+                <div className="socials">
+                    <ul>
 
+                        <a target="_blank" href="https://www.linkedin.com/in/daniela-laiolo-2a4000140/">
+                            <div className="socialLogo">
+                                <img
+                                    src="../assets/images/icons/linkedInLogo.png"
+                                    alt="linkedIn"
+                                />
+                            </div>
+                            <p>Daniela Laiolo</p>
+                        </a>
+
+                        <li>
+                            <div className="socialLogo">
+                                <img
+                                    src="../assets/images/icons/mailLogo.png"
+                                    alt="mailLogo"
+                                />
+                            </div>
+                            <p>danielalaiolo95@gmail.com</p>
+                        </li>
+                        <li>
+                            <div className="socialLogo">
+                                <img
+                                    src="../assets/images/icons/locationLogo.png"
+                                    alt="locationLogo"
+                                />
+                            </div>
+                            <p>Buenos Aires, Argentina</p>
+                        </li>
+                    </ul>
+                </div>
             </div >
             <div className="contactCover">
                 <img
